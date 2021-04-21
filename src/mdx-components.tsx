4d4a6e1cf5233946ae043b0e3167ae7b3d172af4
Matias Meno <m@tias.me>
2021-04-21 17:23:59 +0200

    Add "Copy to clipboard" functionality (#19)
    * Improve design of tabs
    
    * Improve Table of contents
    
    * Fix link style in some cases
    
    * Make table of contents sticky
    
    * Hide Table of contents at a certain breakpoint
    
    * Make sure content shrinks appropriately
    
    * Making sure fonts are always correct in content
    
    * Use CSS to hide table of contents
    
    * Add mobile layout
    
    * Add copy feature
    
    * Remove comment

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,13 +2,14 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
-import { CodeBlock } from "@toitware/code-block";
-import { Link, makeStyles, Typography } from "@material-ui/core";
+import { Link, Typography } from "@material-ui/core";
 import { MDXProviderComponentsProp } from "@mdx-js/react";
+import { CodeBlock } from "@toitware/code-block";
 import React from "react";
-import Title from "./components/Title";
+import Code from "./components/Code";
 import Note from "./components/Note";
 import { Tabs } from "./components/Tabs";
+import Title from "./components/Title";
 
 const P = (props: any) => <Typography paragraph {...props} />;
 const H1 = (props: any) => <Typography variant="h1" {...props} />;
@@ -20,21 +21,6 @@ const H6 = (props: any) => <Typography variant="h6" {...props} />;
 const A = (props: any) => <Link variant="body1" color="textSecondary" {...props} />;
 
 const Pre = (props: any) => <>{props.children}</>;
-
-const useStyles = makeStyles(() => ({
-  code: {
-    fontSize: "0.875em",
-  },
-}));
-
-const Code = (props: { children: React.ReactNode; className?: string }) => {
-  const classes = useStyles();
-  let mode: string | undefined;
-  if (props.className && props.className.startsWith("language-")) {
-    mode = props.className.replace("language-", "");
-  }
-  return <CodeBlock mode={mode} className={classes.code} code={props.children?.toString() ?? ""} />;
-};
 
 export const components: MDXProviderComponentsProp = {
   p: P,
