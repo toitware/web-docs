3ef84106207692d3983cbfc4df92000f02713120
Matias Meno <m@tias.me>
2021-04-22 16:05:30 +0200

    Add proper link support (#36)

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,11 +2,12 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
-import { Link, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import { MDXProviderComponentsProp } from "@mdx-js/react";
 import { CodeBlock } from "@toitware/code-block";
 import React from "react";
 import Code from "./components/Code";
+import { DocsLink } from "./components/DocsLink";
 import Note from "./components/Note";
 import { Tabs } from "./components/Tabs";
 import Title from "./components/Title";
@@ -18,7 +19,6 @@ const H3 = (props: any) => <Typography variant="h3" {...props} />;
 const H4 = (props: any) => <Typography variant="h4" {...props} />;
 const H5 = (props: any) => <Typography variant="h5" {...props} />;
 const H6 = (props: any) => <Typography variant="h6" {...props} />;
-const A = (props: any) => <Link variant="body1" color="textSecondary" {...props} />;
 
 const Pre = (props: any) => <>{props.children}</>;
 
@@ -30,7 +30,7 @@ export const components: MDXProviderComponentsProp = {
   h4: H4,
   h5: H5,
   h6: H6,
-  a: A,
+  a: DocsLink,
   code: Code,
   pre: Pre,
 };
