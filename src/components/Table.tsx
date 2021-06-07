683dedf657d248753d47001adf00e737a1c50b3e
Anders Johnsen <anders@toitware.com>
2021-06-07 10:54:36 +0200

    Style tables

---
+++ src/components/Table.tsx
@@ -0,0 +1,40 @@
+import { Table as MUITable, TableCell, TableContainer, Paper, makeStyles } from '@material-ui/core';
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  tableHead: {
+    fontWeight: 700,
+  },
+}));
+
+type TableProps = {
+  children: React.ReactNode;
+};
+
+export function Table({ children }: TableProps): JSX.Element {
+  return (
+    <TableContainer component={Paper}>
+      <MUITable>
+        {children}
+      </MUITable>
+    </TableContainer>
+  );
+}
+
+export function TableHead({ children }: TableProps): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <TableCell className={classes.tableHead} variant="head">
+      {children}
+    </TableCell>
+  );
+}
+
+export function TableBody({ children }: TableProps): JSX.Element {
+  return (
+    <TableCell variant="body">
+      {children}
+    </TableCell>
+  );
+}
