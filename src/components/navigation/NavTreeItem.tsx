e2ce19f88bb4609663b0cb6cb776f840cadbfc33
Matias Meno <m@tias.me>
2021-11-01 13:59:46 +0100

    Animated main menu

--- src/components/navigation/NavTreeItem.tsx
+++ src/components/navigation/NavTreeItem.tsx
@@ -1,4 +1,4 @@
-import { makeStyles } from "@material-ui/core";
+import { Collapse, makeStyles } from "@material-ui/core";
 import clsx from "clsx";
 import { Link } from "gatsby";
 import * as React from "react";
@@ -9,34 +9,21 @@ import { golden } from "../../theme";
 import NavTree from "./NavTree";
 
 const useStyles = makeStyles((theme) => ({
+  li: {
+    margin: "1rem 0",
+  },
   link: {
     color: "white",
     fontFamily: theme.typography.fontFamily,
     display: "flex",
     alignItems: "center",
     lineHeight: "1.5rem",
-    margin: "0.5rem 0",
   },
-  titleIcon: {
+  icon: {
     marginRight: "1rem",
-  },
-  groupTitle: {
-    margin: "1.5rem 0",
-    cursor: "pointer",
-  },
-  subGroupTitle: {
-    margin: "1rem 0",
-  },
-  subPages: {},
-  subPages1: {
-    borderLeft: "1px solid rgba(255, 255, 255, 0.5)",
-  },
-  activeTitle: {
-    color: golden.toString(),
   },
   active: {
     color: golden.toString(),
-    opacity: 1,
   },
 }));
 
@@ -46,72 +33,28 @@ type ItemProps = {
 };
 
 function NavTreeItem({ page, level }: ItemProps): JSX.Element {
-  const hasSubPages = !!page.children;
-
-  if (!hasSubPages) {
-    return <LeafItem page={page} />;
-  } else {
-    return <GroupItem page={page} level={level} />;
-  }
-}
-
-/**
- * This is a "leaf" entry: it doesn't have subpages so it is a direct link to a
- * page.
- */
-function LeafItem({ page }: { page: MenuItem }): JSX.Element {
   const classes = useStyles();
-
-  const Content = (
-    <>
-      <Icon page={page} />
-      {page.name}
-    </>
-  );
-  return (
-    <li key={page.path}>
-      {!page.href && (
-        <Link to={page.path} className={classes.link} activeClassName={classes.active}>
-          {Content}
-        </Link>
-      )}
-      {page.href && (
-        <a href={page.href} target="_blank" rel="noreferrer" className={classes.link}>
-          {Content}
-        </a>
-      )}
-    </li>
-  );
-}
-
-/**
- * This is a group entry. It does not link to a page.
- */
-function GroupItem({ page, level }: { page: MenuItem; level: number }): JSX.Element {
-  const classes = useStyles();
-
   const currentPath = useSanitizedPath();
-  const isActive = currentPath.startsWith(page.path);
+  const isActive =
+    (page.path == "/" && currentPath == page.path) || (page.path != "/" && currentPath.startsWith(page.path));
 
   const subPages = page.children ?? [];
 
   return (
-    <li key={page.path}>
+    <li key={page.path} className={classes.li}>
       <Link
         to={page.path}
         className={clsx(classes.link, {
-          [classes.groupTitle]: level === 0,
-          [classes.subGroupTitle]: level > 0,
-          [classes.activeTitle]: isActive,
+          [classes.active]: isActive,
         })}
       >
         <Icon page={page} />
         {page.name}
       </Link>
-      {isActive && (
-        <div className={clsx(classes.subPages, { [classes.subPages1]: level == 1 })}>
+      {subPages.length > 0 && (
+        <Collapse in={isActive}>
           <NavTree pages={subPages} level={level + 1} />
-        </div>
+        </Collapse>
       )}
     </li>
   );
@@ -124,21 +67,21 @@ const Icon = ({ page }: { page: MenuItem }): JSX.Element => {
 
   switch (page.icon) {
     case "home":
-      return <FiHome className={classes.titleIcon} />;
+      return <FiHome className={classes.icon} />;
     case "apis":
-      return <FiCloud className={classes.titleIcon} />;
+      return <FiCloud className={classes.icon} />;
     case "language":
-      return <FiCode className={classes.titleIcon} />;
+      return <FiCode className={classes.icon} />;
     case "hardware":
-      return <FiGrid className={classes.titleIcon} />;
+      return <FiGrid className={classes.icon} />;
     case "platform":
-      return <FiLayers className={classes.titleIcon} />;
+      return <FiLayers className={classes.icon} />;
     case "getstarted":
-      return <FiPlay className={classes.titleIcon} />;
+      return <FiPlay className={classes.icon} />;
     case "troubleshoot":
-      return <FiTool className={classes.titleIcon} />;
+      return <FiTool className={classes.icon} />;
     case "tutorials":
-      return <FiBookOpen className={classes.titleIcon} />;
+      return <FiBookOpen className={classes.icon} />;
     case undefined:
       return <></>;
   }
